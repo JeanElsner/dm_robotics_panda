bf683a553bba900c4ad529cf61d3905988eb38ab
JeanElsner <jean.elsner@googlemail.com>
2023-11-22 23:18:46 +0100

    Additional tests

--- test/test_arm.py
+++ test/test_arm.py
@@ -1,9 +1,20 @@
 from dm_control import mjcf
+from numpy import testing
 
-from dm_robotics.panda import arm
+from dm_robotics.panda import arm, parameters
 
 
 def test_physics_step():
   robot = arm.Panda()
   physics = mjcf.Physics.from_mjcf_model(robot.mjcf_model)
   physics.step()
+
+
+def test_set_joint_angles():
+  robot = arm.Panda()
+  robot_params = parameters.RobotParams()
+  physics = mjcf.Physics.from_mjcf_model(robot.mjcf_model)
+  physics.step()
+  robot.set_joint_angles(physics, robot_params.joint_positions)
+  testing.assert_allclose(robot_params.joint_positions,
+                          physics.bind(robot.joints).qpos)
--- test/test_gripper.py
+++ test/test_gripper.py
@@ -1,4 +1,6 @@
+import numpy as np
 from dm_control import mjcf
+from numpy import testing
 
 from dm_robotics.panda import gripper
 
@@ -7,3 +9,19 @@ def test_physics_step():
   robot = gripper.PandaHand()
   physics = mjcf.Physics.from_mjcf_model(robot.mjcf_model)
   physics.step()
+
+  robot = gripper.DummyHand()
+  physics = mjcf.Physics.from_mjcf_model(robot.mjcf_model)
+  physics.step()
+
+
+def test_set_width():
+  robot = gripper.PandaHand()
+  physics = mjcf.Physics.from_mjcf_model(robot.mjcf_model)
+  physics.step()
+
+  robot.set_width(physics, 0)
+  testing.assert_allclose(physics.bind(robot.joints).qpos, np.zeros(2))
+
+  robot.set_width(physics, 0.08)
+  testing.assert_allclose(physics.bind(robot.joints).qpos, 0.04 * np.ones(2))
