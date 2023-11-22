21802221246cca4a2114d31b2d0504fcf2c415e7
JeanElsner <jean.elsner@googlemail.com>
2023-11-22 02:57:43 +0100

    Update rl environment example

--- examples/rl_environment.py
+++ examples/rl_environment.py
@@ -3,18 +3,15 @@ import typing
 
 import dm_env
 import numpy as np
-from dm_control import composer, mjcf
-from dm_control.composer.variation import distributions
+from dm_control import mjcf
+from dm_control.composer.variation import distributions, rotations
 from dm_env import specs
 from dm_robotics.agentflow import spec_utils
-from dm_robotics.agentflow.preprocessors import (observation_transforms,
-                                                 rewards,
-                                                 timestep_preprocessor)
+from dm_robotics.agentflow.preprocessors import rewards, timestep_preprocessor
 from dm_robotics.geometry import pose_distribution
 from dm_robotics.manipulation.props.rgb_objects import rgb_object
-from dm_robotics.moma import entity_initializer, prop, robot, sensor
-from dm_robotics.moma.models.arenas import empty
-from dm_robotics.moma.sensors import prop_pose_sensor
+from dm_robotics.moma import entity_initializer, prop
+from dm_robotics.moma.sensors import camera_sensor, prop_pose_sensor
 
 from dm_robotics.panda import environment
 from dm_robotics.panda import parameters as params
@@ -36,13 +33,17 @@ class Agent:
     action = np.zeros(shape=self._spec.shape, dtype=self._spec.dtype)
     action[:3] = v
     action[6] = 1
+
     return action
 
 
 class Ball(prop.Prop):
+  """Simple ball prop that consists of a MuJoco sphere geom."""
 
   def _build(self, *args, **kwargs):
+    del args, kwargs
     mjcf_root = mjcf.RootElement()
+    # Props need to contain a body called prop_root
     body = mjcf_root.worldbody.add('body', name='prop_root')
     body.add('geom',
              type='sphere',
@@ -79,6 +80,7 @@ if __name__ == '__main__':
   robot_params = params.RobotParams(robot_ip=args.robot_ip)
   panda_env = environment.PandaEnvironment(robot_params)
 
+  # Use the robot added by PandaEnvironment to add a MuJoCo camera element to the gripper.
   panda_env.robots[robot_params.name].gripper.tool_center_point.parent.add(
       'camera',
       pos=(.1, 0, -.1),
@@ -86,41 +88,54 @@ if __name__ == '__main__':
       fovy=90,
       name='wrist_camera')
 
+  # Uniform distribution of 6D poses within the given bounds.
   gripper_pose_dist = pose_distribution.UniformPoseDistribution(
       min_pose_bounds=np.array(
           [0.5, -0.3, 0.7, .75 * np.pi, -.25 * np.pi, -.25 * np.pi]),
       max_pose_bounds=np.array(
           [0.1, 0.3, 0.1, 1.25 * np.pi, .25 * np.pi / 2, .25 * np.pi]))
+  # The pose initializer uses the robot arm's position_gripper function
+  # to set the end-effector pose from the distribution above.
   initialize_arm = entity_initializer.PoseInitializer(
       panda_env.robots[robot_params.name].position_gripper,
       gripper_pose_dist.sample_pose)
 
+  # ComputeReward is a timestep preprocessor that accepts a callable which computes
+  # a scalar reward based on the observation and adds it to the timestep.
+  # We configure the validation frequency so this reward is computed for every timestep.
   reward = rewards.ComputeReward(
       goal_reward,
       validation_frequency=timestep_preprocessor.ValidationFrequency.ALWAYS)
 
-  from dm_robotics.moma.sensors import camera_sensor
-
+  # Extra camera sensor that adds camera observations including rendered images.
   cam_sensor = camera_sensor.CameraImageSensor(
-      panda_env._arena.mjcf_model.find('camera', 'panda/panda_gripper/wrist_camera'),
-      camera_sensor.CameraConfig(has_depth=True), 'wrist_cam')
+      panda_env.robots[robot_params.name].gripper.mjcf_model.find(
+          'camera', 'wrist_camera'), camera_sensor.CameraConfig(has_depth=True),
+      'wrist_cam')
 
+  # Instantiate props
   ball = Ball()
   props = [ball]
   for i in range(5):
     props.append(rgb_object.RandomRgbObjectProp(color=(.5, .5, .5, 1)))
 
+  # Props need to be added to the environment before instantiating the prop initializer.
   panda_env.add_props(props)
 
+  # This prop initializer uses uniform random samples within the interval [-0.5, 0.5] for
+  # positions and uniformly distributed random quaternion samples for orientation. The initializer
+  # by default keeps sampling poses for props until a collision-free pose is found.
   initialize_props = entity_initializer.prop_initializer.PropPlacer(
-      props, distributions.Uniform(0, .5))
+      props, distributions.Uniform(-.5, .5), rotations.UniformQuaternion())
 
   panda_env.add_timestep_preprocessors([reward])
   panda_env.add_entity_initializers([
       initialize_arm,
       initialize_props,
   ])
-  panda_env.add_extra_sensors([cam_sensor, prop_pose_sensor.PropPoseSensor(ball, 'goal')])
+  # Extra sensors include a camera sensor as well as a pose sensor for the ball.
+  panda_env.add_extra_sensors(
+      [cam_sensor, prop_pose_sensor.PropPoseSensor(ball, 'goal')])
 
   with panda_env.build_task_environment() as env:
     # Print the full action, observation and reward specification
