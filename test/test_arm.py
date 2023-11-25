6bd75d5500bdf6763237b043780ba82abf1f3183
JeanElsner <jean.elsner@googlemail.com>
2023-11-25 04:26:18 +0100

    Add custom gripper test

--- test/test_arm.py
+++ test/test_arm.py
@@ -1,5 +1,8 @@
 import numpy as np
 from dm_control import mjcf
+from dm_robotics.moma.effectors import default_gripper_effector
+from dm_robotics.moma.models.end_effectors.robot_hands import robotiq_2f85
+from dm_robotics.moma.sensors import robotiq_gripper_sensor
 from numpy import testing
 
 from dm_robotics.panda import arm, arm_constants, parameters
@@ -27,6 +30,7 @@ def test_arm_effector():
   physics = mjcf.Physics.from_mjcf_model(robot.mjcf_model)
   effector = arm.ArmEffector(robot_params, robot)
   effector.set_control(physics, np.zeros(7, dtype=np.float32))
+  robot.after_substep(physics, np.random.RandomState(0))
   effector.close()
 
 
@@ -37,3 +41,15 @@ def test_arm_haptic():
   physics = mjcf.Physics.from_mjcf_model(robot.mjcf_model)
   effector = arm.ArmEffector(robot_params, robot)
   effector.set_control(physics, np.zeros(0, dtype=np.float32))
+
+
+def test_custom_gripper():
+  gripper = robotiq_2f85.Robotiq2F85()
+  gripper_params = parameters.GripperParams(
+      model=gripper,
+      effector=default_gripper_effector.DefaultGripperEffector(
+          gripper, 'robotique'),
+      sensors=[
+          robotiq_gripper_sensor.RobotiqGripperSensor(gripper, 'robotique')
+      ])
+  arm.build_robot(parameters.RobotParams(gripper=gripper_params))
