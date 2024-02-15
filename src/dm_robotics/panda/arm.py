eaba1af61ac863fd4867a7b85e8409bfdf8d4b52
JeanElsner <jean.elsner@googlemail.com>
2024-02-15 16:26:37 +0100

    chore: formatting

--- src/dm_robotics/panda/arm.py
+++ src/dm_robotics/panda/arm.py
@@ -358,7 +358,7 @@ class Cartesian6dVelocityEffector(
 
 class ArmEffector(arm_effector.ArmEffector):
   """Robot arm effector for the Panda MoMa model.
-  
+
   Takes :py:class:`dm_robotics.panda.parameters.RobotParams`
   and changes the joint stiffness and damping of the robot arm. Otherwise behaves
   like :py:class:`dm_robotics.moma.effectors.arm_effector.ArmEffector`.
@@ -466,7 +466,7 @@ class RobotTCPSensor(site_sensor.SiteSensor):
 
 class RobotArmSensor(robot_arm_sensor.RobotArmSensor):
   """Behaves like :py:class:`dm_robotics.moma.sensors.robot_arm_sensor.RobotArmSensor`.
-  
+
   Except that the joint torque signal does not include passive forces.
   This is done so as to model the external torque signal provided by the Panda robot.
   """
